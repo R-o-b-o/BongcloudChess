ffbaa727fd13c82d28aa7bf5045309abbc79429c
R-o-b-o <39059788+R-o-b-o@users.noreply.github.com>
2020-07-12 16:16:53 +0100

    UI/UX changes + bugfixes

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,30 @@
 var is2player;
+const isBlackBot = Math.floor(Math.random()*2);
 var modal = document.getElementById("modal");
+var boardHistory = [];
+
+//document.body.style.background = '#'+`000000${Math.random().toString(16)}`.substr(-6); /* generate random color as bg */
+
+var lightTileCol = "#cfcfcf";
+var darkTileCol = "#614796";
+var boardBorderCol = "#3f3f3f";
+
+var lightTheme = 0 == Math.floor(Math.random()*20);
+
+if (lightTheme) { 
+    document.body.style.background = "#d8a2cf";
+    document.getElementById("modal-content").style.backgroundColor = "rgba(60, 60, 60, 0.85)";
+    lightTileCol = "#fff";
+    darkTileCol = "#fcb8ea";
+    boardBorderCol = "#fff"
+}
+
 
 document.getElementById("playbot").onclick = function () {
     hideModal();
     is2player = false;
+    
+    if (isBlackBot) { setTimeout(MakeAiMove, 100); }
 }
 document.getElementById("2player").onclick = function () {
     hideModal();
@@ -11,9 +32,18 @@ document.getElementById("2player").onclick = function () {
 }
 
 function hideModal() {
-    modal.style.display = "none";
-    document.getElementById("2player").style.display = "none";
-    document.getElementById("playbot").style.display = "none";
+    if (!lightTheme) { document.body.style.background = "#221c25"; }
+    modal.style.visibility = "hidden";
+    document.getElementById("modal-content").style.visibility = "visible";
+    document.getElementById("modal-content").classList.add("hide");
+ 
+    setTimeout(function () {
+        document.getElementById("modal-content").classList.remove("hide");
+        document.getElementById("2player").style.display = "none";
+        document.getElementById("playbot").style.display = "none";
+        modal.style.visibility = "visible";
+        modal.style.display = "none";
+    }, 300)
 }
 
 const canvas = document.getElementById("board");
@@ -26,7 +56,13 @@ function resizeCanvas() {
     refresh();
 }
 
-GetBoard = Module.cwrap('get_board', 'string', [null]);
+GetBoardWasm = Module.cwrap('get_board', 'string', [null]);
+
+function GetBoard() {
+    let board = GetBoardWasm().slice(0, 64);
+    if (isBlackBot) { board = [...board].reverse().join(""); }
+    return board;
+}
 
 AiMove = Module.cwrap('ai_move', null, ['number']);
 UserMove = Module.cwrap('user_move', 'number', ['number', 'number']);
@@ -40,7 +76,7 @@ function onRuntimeInitialized() {
 }
 
 function refresh() {
-    var board = GetBoard().slice(0, -1);
+    let board = GetBoard();
     DrawBoard(canvas, board);
 }
 
@@ -50,25 +86,33 @@ var ply = 1;
 function movePiece(canvas, event) {
     const rect = canvas.getBoundingClientRect()
 
-    var tileSize = canvas.width / 8;
-    var xSquare = Math.floor((event.clientX - rect.left) / tileSize);
-    var ySquare = Math.floor((event.clientY - rect.top) / tileSize);
+    let tileSize = canvas.width / 8;
+    let xSquare = Math.floor((event.clientX - rect.left) / tileSize);
+    let ySquare = Math.floor((event.clientY - rect.top) / tileSize);
 
-    var piece = GetBoard().slice(0, -1)[ySquare * 8 + xSquare];
+    let piece = GetBoard()[ySquare * 8 + xSquare];
 
     if ((ply % 2) == (piece == piece.toUpperCase()) && piece != '.') {
         refresh();
 
         move[0] = ySquare * 8 + xSquare;
-        var offset = Module._malloc(5 * 4)
-        var squares = Module.HEAP32.subarray(offset / 4, offset / 4 + 5);
+
+        if (isBlackBot) { move[0] = 63 - move[0]; }
+
+        let offset = Module._malloc(5 * 4)
+        let squares = Module.HEAP32.subarray(offset / 4, offset / 4 + 5);
         GetSquares(move[0], offset);
+
+        if (isBlackBot) { squares = squares.map(x => 63 - x); }
 
         DrawMove(canvas, piece, squares, xSquare, ySquare);
         Module._free(offset);
     } else {
         move[1] = ySquare * 8 + xSquare;
-        var valid = UserMove(move[0], move[1]);
+
+        if (isBlackBot) { move[1] = 63 - move[1]; }
+
+        let valid = UserMove(move[0], move[1]);
         requestAnimationFrame(refresh);
         if (valid) {
             ply++;
@@ -82,7 +126,7 @@ function movePiece(canvas, event) {
 }
 
 function MakeAiMove() {
-    var start = Date.now();
+    let start = Date.now();
     ply++;
     AiMove(depth);
     if (Date.now() - start < 300) { depth++; }
@@ -92,37 +136,45 @@ function MakeAiMove() {
 }
 
 function endGame() {
-    if (Module.ccall('is_mainboard_terminal', 'number', [null], [])) {
+    let threefoldRep = [GetBoard(), boardHistory[3], boardHistory[7]].reduce((acc, cur, _, src) => acc = cur == src[0]);
+    if (Module.ccall('is_mainboard_terminal', 'number', [null], []) || threefoldRep) {
         refresh();
         setTimeout(function () {
-            var score = Module.ccall('score_mainboard', 'number', [null], []);
-            var elem = document.createElement("p");
-            var btn = document.createElement("button");
+            const score = Module.ccall('score_mainboard', 'number', [null], []);
+            let elem = document.createElement("p");
+            let btn = document.createElement("button");
             btn.innerHTML = "play again";
             
             document.getElementById("modal-content").appendChild(elem);
             document.getElementById("modal-content").appendChild(btn);
             modal.style.display = "block";
             
-            if (score == 1) {
+            let color, textColor;
+            if (score == 100) {
                 elem.innerHTML = "White Wins!";
-                var color = "#ff45a2";
-            } else if (score == -1) {
+                textColor = "#000"
+                color = "#ff7dbe";
+            } else if (score == -100) {
                 elem.innerHTML = "Black Wins!";
-                var color = "#6832a8";
+                textColor = "#fff"
+                color = "#6832a8";
             } else {
                 elem.innerHTML = "Draw!";
-                var color = "#9198e5";
+                textColor = "#dcdcdc";
+                color = "#595fa5";
             }
-            document.getElementById("modal-content").style = "background-color: "+color+"; margin: 10% auto;"
+            document.getElementById("modal-content").style = `background-color: ${color}; margin: 10% auto; color: ${textColor};`
+            
             elem.style = "font-size: 60px;"
-            btn.style = "font-size: 18px;"
+            btn.classList.add("button-small") 
             btn.onclick = function () {
                 location.reload();
             }
         }, 0);
         return true;
     }
+    boardHistory.push(GetBoard());
+    boardHistory = boardHistory.slice(0, 8);
     return false;
 }
 
@@ -145,10 +197,10 @@ function checkLoaded() {
 }
 
 function DrawBoard(can, board) {
-    var ctx = can.getContext("2d");
-    var w = can.width;
-    var h = can.height;
-    ctx.fillStyle = "#cfcfcf";
+    let ctx = can.getContext("2d");
+    let w = can.width;
+    let h = can.height;
+    ctx.fillStyle = lightTileCol;
     ctx.fillRect(0, 0, w, h);
 
     nRow = nCol = 8;
@@ -156,23 +208,23 @@ function DrawBoard(can, board) {
     w /= nCol;
     h /= nRow;
 
-    for (var i = 0; i < nRow; ++i) {
-        for (var j = 0; j < nCol; ++j) {
+    for (let i = 0; i < nRow; ++i) {
+        for (let j = 0; j < nCol; ++j) {
             if (j % 2 == 0) {
-                ctx.fillStyle = "#614796";
+                ctx.fillStyle = darkTileCol;
                 ctx.fillRect(j * w + (i % 2 ? 0 : w), i * h, w, h);
             }
 
-            var char = board[i * 8 + j];
+            let char = board[i * 8 + j];
             if (char != '.') { DrawImage(char, ctx, j * w, i * h, w, h); }
         }
     }
-    ctx.strokeStyle = "#3f3f3f";
+    ctx.strokeStyle = boardBorderCol;
     ctx.lineWidth = "5";
     ctx.beginPath();
     ctx.rect(0, 0, can.width, can.height);
     ctx.stroke();
-    ctx.strokeStyle = "#000"
+    ctx.strokeStyle = "#000";
 }
 
 function DrawImage(char, ctx, x1, y1, x2, y2) {
@@ -188,8 +240,8 @@ function DrawImage(char, ctx, x1, y1, x2, y2) {
 }
 
 function DrawMove(canvas, piece, squares, xSquare, ySquare) {
-    var ctx = canvas.getContext("2d");
-    var tileSize = canvas.width / 8;
+    let ctx = canvas.getContext("2d");
+    let tileSize = canvas.width / 8;
     ctx.lineWidth = "3";
 
     ctx.fillStyle = "#241430";
